3cc4da2de5e48939fd446be25b022686bece24ef
Rahul Dhiman <rahul.dhiman.mohanlal@gmail.com>
2026-07-16 16:43:56 +0530

    updated

--- platform/plugins/ecommerce/src/Tables/QuoteTable.php
+++ platform/plugins/ecommerce/src/Tables/QuoteTable.php
@@ -3,6 +3,7 @@
 namespace Botble\Ecommerce\Tables;
 
 use Botble\Base\Facades\BaseHelper;
+use Botble\Base\Facades\Html;
 use Botble\Ecommerce\Enums\QuoteStatusEnum;
 use Botble\Ecommerce\Models\QuoteRequest;
 use Botble\Table\Abstracts\TableAbstract;
@@ -13,15 +14,18 @@ use Botble\Table\BulkChanges\CreatedAtBulkChange;
 use Botble\Table\BulkChanges\EmailBulkChange;
 use Botble\Table\BulkChanges\NameBulkChange;
 use Botble\Table\BulkChanges\StatusBulkChange;
+use Botble\Table\Columns\Column;
 use Botble\Table\Columns\CreatedAtColumn;
 use Botble\Table\Columns\EmailColumn;
-use Botble\Table\Columns\FormattedColumn;
 use Botble\Table\Columns\IdColumn;
 use Botble\Table\Columns\NameColumn;
 use Botble\Table\Columns\StatusColumn;
-use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Relations\Relation;
+use Illuminate\Database\Query\Builder as QueryBuilder;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Validation\Rule;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -36,48 +40,62 @@ class QuoteTable extends TableAbstract
                     ->route('quote-requests.show')
                     ->permission('quote-requests.index'),
                 DeleteAction::make()->route('quote-requests.destroy'),
-            ])
-            ->queryUsing(function (Builder $query) {
-                return $query
-                    ->select([
-                        'id',
-                        'product_id',
-                        'name',
-                        'email',
-                        'phone',
-                        'company_name',
-                        'quantity',
-                        'status',
-                        'created_at',
-                    ])
-                    ->with(['product:id,name']);
+            ]);
+    }
+
+    public function ajax(): JsonResponse
+    {
+        $data = $this->table
+            ->eloquent($this->query())
+            ->editColumn('name', function (QuoteRequest $item) {
+                return BaseHelper::clean($item->name);
+            })
+            ->editColumn('product_id', function (QuoteRequest $item) {
+                if (! empty($item->product) && $item->product->name) {
+                    return BaseHelper::clean($item->product->name);
+                }
+
+                return 'N/A';
             });
+
+        return $this->toJson($data);
+    }
+
+    public function query(): Relation|Builder|QueryBuilder
+    {
+        $query = $this->getModel()
+            ->query()
+            ->select([
+                'id',
+                'product_id',
+                'name',
+                'email',
+                'phone',
+                'company_name',
+                'quantity',
+                'status',
+                'created_at',
+            ])
+            ->with(['product:id,name']);
+
+        return $this->applyScopes($query);
     }
 
     public function columns(): array
     {
         return [
             IdColumn::make(),
-            NameColumn::make()
+            Column::make('name')
                 ->title(trans('plugins/ecommerce::quote-requests.customer_name'))
-                ->route('quote-requests.show')
                 ->alignStart(),
             EmailColumn::make(),
-            NameColumn::make('company_name')
+            Column::make('company_name')
                 ->title(trans('plugins/ecommerce::quote-requests.company'))
                 ->alignStart(),
-            FormattedColumn::make('product_id')
+            Column::make('product_id')
                 ->title(trans('plugins/ecommerce::quote-requests.product'))
-                ->alignStart()
-                ->renderUsing(function (FormattedColumn $column) {
-                    $item = $column->getItem();
-                    if ($item->product) {
-                        return BaseHelper::clean($item->product->name);
-                    }
-
-                    return 'N/A';
-                }),
-            NameColumn::make('quantity')
+                ->alignStart(),
+            Column::make('quantity')
                 ->title(trans('plugins/ecommerce::quote-requests.quantity'))
                 ->alignStart(),
             StatusColumn::make(),
