d4cf4d8012fe3d2af0bc367bd2c0b699847d69b1
Rahul Dhiman <rahul.dhiman.mohanlal@gmail.com>
2026-07-16 23:01:03 +0530

    updated

--- platform/themes/martfury/partials/short-codes/download-app.blade.php
+++ platform/themes/martfury/partials/short-codes/download-app.blade.php
@@ -16,36 +16,37 @@
     </div>
 </div>
 
-@once
-    @push('footer')
-        <script>
-            $(document).ready(function () {
-                if ($.fn.slick) {
-                    $('.banner-carousel').slick({
-                        dots: true,
-                        arrows: true,
-                        infinite: true,
-                        autoplay: true,
-                        autoplaySpeed: 4000,
-                        speed: 600,
-                        slidesToShow: 1,
-                        slidesToScroll: 1,
-                        fade: true,
-                        cssEase: 'linear',
-                        prevArrow: '<button type="button" class="slick-prev"><i class="icon-chevron-left"></i></button>',
-                        nextArrow: '<button type="button" class="slick-next"><i class="icon-chevron-right"></i></button>',
-                        responsive: [
-                            {
-                                breakpoint: 768,
-                                settings: {
-                                    arrows: false,
-                                    dots: true
-                                }
-                            }
-                        ]
-                    });
-                }
-            });
-        </script>
-    @endpush
-@endonce
+<script>
+(function() {
+    var el = document.querySelector('.banner-carousel');
+    if (!el) return;
+    function start() {
+        if (typeof jQuery === 'undefined' || !jQuery.fn || !jQuery.fn.slick) {
+            setTimeout(start, 100);
+            return;
+        }
+        jQuery(el).slick({
+            dots: true,
+            arrows: true,
+            infinite: true,
+            autoplay: true,
+            autoplaySpeed: 4000,
+            speed: 600,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            fade: true,
+            cssEase: 'linear',
+            prevArrow: '<button type="button" class="slick-prev"><i class="icon-chevron-left"></i></button>',
+            nextArrow: '<button type="button" class="slick-next"><i class="icon-chevron-right"></i></button>',
+            responsive: [
+                { breakpoint: 768, settings: { arrows: false, dots: true } }
+            ]
+        });
+    }
+    if (document.readyState === 'loading') {
+        document.addEventListener('DOMContentLoaded', start);
+    } else {
+        start();
+    }
+})();
+</script>
