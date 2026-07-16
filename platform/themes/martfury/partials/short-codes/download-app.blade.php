f51d05efe016a526c3c384420b6ff23cfc3e1881
Rahul Dhiman <rahul.dhiman.mohanlal@gmail.com>
2026-07-16 23:28:18 +0530

    updated banner

--- platform/themes/martfury/partials/short-codes/download-app.blade.php
+++ platform/themes/martfury/partials/short-codes/download-app.blade.php
@@ -4,7 +4,6 @@
             @php
                 $banners = [
                     'https://images-static.nykaa.com/uploads/9ff20482-b3a3-47a0-a77e-20a21ebed482.gif',
-                    'https://images-static.nykaa.com/uploads/dadddf26-4051-481e-86da-a75596e538e7.jpg?tr=w-1920',
                 ];
             @endphp
             @foreach ($banners as $banner)
